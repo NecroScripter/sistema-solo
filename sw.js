4b6a5444d417e5666868ecb28da72214ed6bfb1e
NecroScripter <thiagoreck19@gmail.com>
2026-01-14 01:20:34 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,37 @@
-self.addEventListener('fetch', function(event) {
-  // Nada complexo por enquanto, apenas permite o funcionamento básico
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = 'solo-leveling-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  'https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap'
+];
+
+// Instalação: Cacheia os arquivos iniciais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Ativação: Limpa caches antigos se houver atualização
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptação: Tenta servir do cache, se falhar, vai pra internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
 });
